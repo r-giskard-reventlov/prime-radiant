5b54c350abbf88020167b5d9dfe642f463f11894
justin <justinleewilson@gmail.com>
2016-06-07 19:18:19 +0100

    table printing to console passes original test + added extra test on lower boundary

--- src/main/java/co/uk/rgiskardreventov/ConsoleTablePrinter.java
+++ src/main/java/co/uk/rgiskardreventov/ConsoleTablePrinter.java
@@ -4,7 +4,51 @@ package co.uk.rgiskardreventov;
  * Created by justin on 6/7/16.
  */
 public class ConsoleTablePrinter implements TablePrinter {
-    public void printMultiplicationTable(int[] values) {
 
+    public void printMultiplicationTable(int[] values) {
+        if(values.length == 0) {
+            return;
+        }
+        int spacingRequired = calculateCellWidthBasedOnLastValue(values[values.length - 1]);
+        printHeaderRow(values, spacingRequired);
+        printBody(values, spacingRequired);
+    }
+
+    private int calculateCellWidthBasedOnLastValue(int value) {
+        long lastValue = (long)value;
+        long lastValueTableValue = lastValue * lastValue;
+        return String.valueOf(lastValueTableValue).length();
+    }
+
+    private void printEntry(int spacing, int val) {
+        int spacingWithPadding = spacing + 1;
+        String format = "|%" + spacingWithPadding + "d ";
+        System.out.print(String.format(format, val));
+    }
+
+    private void printEntry(int spacing) {
+        int spacingWithPadding = spacing + 1;
+        String format = "|%" + spacingWithPadding + "s ";
+        System.out.print(String.format(format, ""));
+    }
+
+    private void printBody(int[] values, int spacingRequired) {
+        for(int i=0; i<values.length; i++) {
+            for(int j=0; j<values.length; j++) {
+                if(j == 0) {
+                    printEntry(spacingRequired, values[i]);
+                }
+                printEntry(spacingRequired, values[i] * values[j]);
+            }
+            System.out.print("\n");
+        }
+    }
+
+    private void printHeaderRow(int[] values, int spacingRequired) {
+        printEntry(spacingRequired);
+        for(int i=0; i<values.length; i++) {
+            printEntry(spacingRequired, values[i]);
+        }
+        System.out.print("\n");
     }
 }
--- src/test/java/co/uk/rgiskardreventov/ConsoleTablePrinterTest.java
+++ src/test/java/co/uk/rgiskardreventov/ConsoleTablePrinterTest.java
@@ -39,11 +39,38 @@ public class ConsoleTablePrinterTest {
         assertEquals(singleValueTable(), outContent.toString());
     }
 
+    @Test
+    public void printMultiplicationTable_noValues() throws Exception {
+        int[] values = new int[]{};
+        testSubject.printMultiplicationTable(values);
+        assertEquals(noValuesTable(), outContent.toString());
+    }
+
+    @Test
+    public void printMultiplicationTable_fiveValues() throws Exception {
+        int[] values = new int[]{2, 3, 5, 7, 11};
+        testSubject.printMultiplicationTable(values);
+        assertEquals(fiveValueTable(), outContent.toString());
+    }
+
     private String singleValueTable() {
         return
                 "|   | 2 \n" +
                 "| 2 | 4 \n";
+    }
 
+    private String noValuesTable() {
+        return "";
+    }
+
+    private String fiveValueTable() {
+        return
+                "|     |   2 |   3 |   5 |   7 |  11 \n" +
+                "|   2 |   4 |   6 |  10 |  14 |  22 \n" +
+                "|   3 |   6 |   9 |  15 |  21 |  33 \n" +
+                "|   5 |  10 |  15 |  25 |  35 |  55 \n" +
+                "|   7 |  14 |  21 |  35 |  49 |  77 \n" +
+                "|  11 |  22 |  33 |  55 |  77 | 121 \n";
     }
 
 }
